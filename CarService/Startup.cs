42abea2f21e6377d67985c9ed64836d56ba8d4c5
amirfahed777@gmail.com <amirfahed777@gmail.com>
2020-12-06 16:38:42 +0300

    Добавлены зависимости

--- CarService/Startup.cs
+++ CarService/Startup.cs
@@ -2,10 +2,16 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CarService.Database;
+using CarService.Models;
+using CarService.Repositories;
+using CarService.Services.Implementation;
+using CarService.Services.Interfaces;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -25,6 +31,16 @@ namespace CarService
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
+
+            services.AddEntityFrameworkNpgsql().AddDbContext<ApplicationContext>(opt =>
+                    opt.UseNpgsql(Configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddTransient<IRepairService, RepairService>();
+            services.AddTransient<IBaseRepository<Document>, BaseRepository<Document>>();
+            services.AddTransient<IBaseRepository<Car>, BaseRepository<Car>>();
+            services.AddTransient<IBaseRepository<Worker>, BaseRepository<Worker>>();
+
             services.AddControllers();
         }
 
